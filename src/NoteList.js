4a8dcdcac56d2246746d7033b15a4053485472a6
Michelle LaLonde <mlalonde309@gmail.com>
2020-10-06 14:44:10 -0400

    Update NoteList.js

--- src/NoteList.js
+++ src/NoteList.js
@@ -5,9 +5,11 @@ import './NoteList.css';
 export default function NoteList(props){
     const list = props.notes.map((note) => {
             return(
-                <NoteItemList key = {note.id} name= {note.name} />
+                <NoteItemList key = {note.id} name= {note.name} folderId= {note.folderId}/>
             )
         })
+
+
     return (
         <div className= 'notes'>
             <ul>
