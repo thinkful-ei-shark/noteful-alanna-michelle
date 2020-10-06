04468abb8c4de2679c4aeb8d7d2c084a8bd5632b
Alanna Bouloy <lanihope@gmail.com>
2020-10-06 16:45:31 -0400

    fixed FolerList linking issue

--- src/FolderPage.js
+++ src/FolderPage.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import Header from './Header';
-import FolderList from './FolderItem';
+import FolderList from './FolderList';
 import NoteList from './NoteList';
 import store from './store';
+import './FolderPage.css'
 
 
 export default class FolderPage extends React.Component {
     state = {
         folders: store.folders,
-        notes: store.notes
+        notes: store.notes,
     }
 
     render(){
         return (
-        <div class='FolderPage'>
-            <Header />
-            <main>
-                <FolderList folders={this.state.folders}/>
-                <NoteList notes = {this.state.notes} folderId = {this.props.match.params.folderId}/>
-            </main>
-        </div>
+            <div className='FolderPage'>
+                <Header />
+                <main>
+                    <FolderList folders={this.state.folders}/>
+                    <NoteList notes = {this.state.notes} folderId = {this.props.match.params.folderId}/>
+                </main>
+            </div>
         
         )
     }
